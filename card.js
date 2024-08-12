b6abae8b581401af0b6f05ec5aa11a9a668e3deb
AKASH KAPOOR <131011402+Akashkapoor11@users.noreply.github.com>
2024-08-12 23:05:38 +0530

    Create card.js

---
+++ card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Arrow = ({ direction, onClick }) => {
+  const directional = text-primary fas fa-arrow-${direction}
+  return (
+    <div
+      className={slide-arrow ${direction}}
+      onClick={onClick}
+    >
+      <i className={directional} />
+    </div>
+  )
+}
